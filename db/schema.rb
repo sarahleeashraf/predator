082005a57000a4ecc42e9bb2f6de62e2aebc750c
Sarah Ashraf <sarahleeashraf@gmail.com>
2011-01-23 16:00:38 -0500

    enabled sessions in the db

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110121023141) do
+ActiveRecord::Schema.define(:version => 20110123205757) do
 
   create_table "data_points", :force => true do |t|
     t.date     "date",       :null => false
@@ -25,6 +25,16 @@ ActiveRecord::Schema.define(:version => 20110121023141) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "wells", :force => true do |t|
     t.string   "name",       :null => false
