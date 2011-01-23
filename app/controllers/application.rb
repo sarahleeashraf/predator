374fb7b55e8029d3af70db101df428a14c6e6897
Sarah Ashraf <sarahleeashraf@gmail.com>
2011-01-23 15:34:25 -0500

    layout stuff

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,7 +2,9 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  layout 'standard'
   helper :all # include all helpers, all the time
+  
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
@@ -12,4 +14,5 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
+  
 end
